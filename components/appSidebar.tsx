fcf2eef51aff61fe6330f967593e87b98fb2b40d
JonathanJuayong <jonathan.juayong@gmail.com>
2020-12-08 23:31:07 +0800

    styled sidebar

--- components/appSidebar.tsx
+++ components/appSidebar.tsx
@@ -1,24 +1,65 @@
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, Button, Text, background } from '@chakra-ui/react';
 import { useState } from 'react';
+import { Logo } from '../components';
+import { theme } from '../lib/theme';
+import { FiHome, FiMonitor, FiCalendar } from 'react-icons/fi';
 
 export const AppSidebar: React.FC<{}> = ({ children }) => {
-  const [active, setActive] = useState("home");
-  const navItems: Array<{ name: string, link: string }> = [
-    { name: "home", link: "" },
-    { name: "properties", link: "" },
-    { name: "appointments", link: "" },
+  const [active, setActive] = useState("dashboard");
+  const hoverStyle = {
+    color: theme.colors.blue[900],
+    background: "white"
+  }
+  const navItems: Array<{ name: string, link: string, icon: JSX.Element }> = [
+    { name: "dashboard", link: "", icon: <FiMonitor /> },
+    { name: "properties", link: "", icon: <FiHome /> },
+    { name: "appointments", link: "", icon: <FiCalendar /> },
   ];
   return (
     <Grid
       gridAutoFlow="column"
       gridTemplateColumns="2fr 10fr"
+      gap="1em"
     >
       <Grid
+        className="app sidebar"
+        bg={theme.colors.blue[900]}
         minH="100vh"
+        justifyItems="center"
+        alignItems="center"
       >
-        test
+        <GridItem>
+          <Logo
+            height={40}
+            white
+          />
+        </GridItem>
+        <Grid
+          alignSelf="flex-start"
+          w="100%"
+        >
+          {navItems.map((item, i) => (
+            <Button
+              h="5em"
+              key={i}
+              borderRadius="0px"
+              variant="ghost"
+              color="white"
+              fontWeight="400"
+              leftIcon={item.icon}
+              _hover={hoverStyle}
+              style={active === item.name ? hoverStyle : {}}
+              onClick={() => setActive(item.name)}
+            >
+              <Text>{item.name}</Text>
+            </Button>
+          ))}
+        </Grid>
       </Grid>
-      <GridItem>
+      <GridItem
+        className="app content"
+        py="1em"
+      >
         {children}
       </GridItem>
     </Grid>
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
+import { Box } from '@chakra-ui/react';
 
-export const Logo: React.FC<{height: number}> = ({height}) => {
-  const src = "/logo.svg";
+export const Logo: React.FC<{height: number, white?: boolean}> = ({height, white}) => {
+  const src = white ? "/logo-white.svg" : "/logo.svg";
   const alt = "real simple";
   const width = height * 3.42;
   return (
