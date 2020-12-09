13080fcdd655353511e6021e9fd127edc6514714
JonathanJuayong <jonathan.juayong@gmail.com>
2020-12-09 10:57:16 +0800

    added interface

--- components/index.tsx
+++ components/index.tsx
@@ -2,3 +2,13 @@ export { Layout } from './layout';
 export { AppSidebar } from './layout/appSidebar';
 export { Logo } from './logo';
 export { PropertyCard } from './propertyCard';
+
+export interface PropertyCardProps{
+  name?: string,
+  city?: string,
+  price?: number,
+  bed?: number,
+  bath?: number,
+  thumbnail?: string,
+  empty?: boolean,
+}
